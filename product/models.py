799bc875a6176e784e0900bb97b353ee7a6c33b6
imanalopher <iman.mamadiyev@gmail.com>
2017-07-07 01:07:48 +0600

    Product model and add to admin interface

--- product/models.py
+++ product/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=11, decimal_places=5)
+    description = models.CharField(max_length=4040)
+    photo = models.CharField(max_length=1000)
+    amount = models.SmallIntegerField()
+    lastUpdate = models.DateTimeField(auto_now=True, default=datetime.now(), null=False),
+    reviews = models.IntegerField(default=0)
+    rating = models.SmallIntegerField(default=0)
+    code = models.CharField(max_length=100)
