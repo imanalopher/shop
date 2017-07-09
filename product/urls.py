63dd73be975c844f1de387072b3618d97a47b971
imanalopher <iman.mamadiyev@gmail.com>
2017-07-09 23:59:19 +0600

    Category Page

--- product/urls.py
+++ product/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='product.index'),
+    url(r'^(?P<id>[0-9]+)$', views.category, name='category.getById'),
 ]
--- product/views.py
+++ product/views.py
@@ -6,3 +6,9 @@ def index(request):
     products = Product.objects.all()[:10]
     categories = Category.objects.filter(active=True, parent__isnull=True)
     return render(request, 'product/index.html', {'products': products, 'categories': categories})
+
+
+def category(request, id):
+    categories = Category.objects.filter(active=True, parent__isnull=True)
+    products = Product.objects.filter(category_id=id)
+    return render(request, 'product/category.html', {'products': products, 'categories': categories})
